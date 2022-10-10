4f27f6eb710509b1d7c9284addb94b5e8a1d0ebc
llove5 <ldlove.uncg.edu>
2022-10-10 17:22:53 -0400

    added Plant files

---
+++ demo (4)/demo/src/main/java/com/FinalTest/demo/plant/PlantRepository.java
@@ -0,0 +1,12 @@
+
+package com.FinalTest.demo.plant;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ *
+ * @author llove
+ */
+public interface PlantRepository extends JpaRepository<Plant, Long>{
+    
+}
